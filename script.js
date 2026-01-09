4141951b3d0d0ad38d482d132e01115f5423411c
Alearaldi <37593361+alearaldi@users.noreply.github.com>
2026-01-09 19:19:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Animación simple para mostrar las imágenes al hacer scroll
 window.addEventListener('scroll', () => {
-    const images = document.querySelectorAll('.grid-gallery img');
+    const images = document.querySelectorAll('.gallery img');
     images.forEach(img => {
         const speed = 2;
         const rect = img.getBoundingClientRect();
